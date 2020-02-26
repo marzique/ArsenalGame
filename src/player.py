77ae7f1e00420e0ceac282b40eac674e67f9daa2
Danny Tarn <marzique@gmail.com>
2020-02-26 15:57:17 +0200

    refactor to function/Player class

---
+++ src/player.py
@@ -0,0 +1,10 @@
+"""Player data"""
+
+
+
+class Player:
+	def __init__(self, nickname):
+		self.health = 100
+		self.armor = 100
+
+
---
+++ src/untitled folder/game.py
@@ -0,0 +1,8 @@
+"""Represent state of actual game world. 
+Handle updating world info, rendering everythibg to screen, 
+handling player events.
+"""
+
+
+class Game(states.States):
+	pass
