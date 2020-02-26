a6ec534cde22a2359dc7de043e43c36390fb5f85
marzique <marzique@gmail.com>
2020-02-27 01:28:45 +0200

    proper clock and show FPS

--- doom.py
+++ doom.py
@@ -10,6 +10,7 @@ https://gist.github.com/limdingwen/c8bb49474de7765f92ee198a1f0f31d5
 import pygame
 import math
 import time
+from src.player import Player
 
 
 WIDTH = 1400
@@ -44,24 +45,19 @@ PLAYER_RAY_COLOR = (255, 0, 0)
 CEILING_COLOR = (100, 130, 200)
 FLOOR_COLOR = (10, 10, 10)
 
-FRAME_MIN = 0.016
+FPS = 60
 
 MOUSE_SENSITIVITY = 0.15
 
 
-
-class Player:
-    def __init__(self):
-        self.pos = (0.0, 0.0) 
-        self.rot = 0.0 # 0 is north, clockwise, degrees
-
-
-player = Player()
+player = Player('tarn')
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("monospace", 15)
 
-# invisible cursor?
+# hide cursor, and keep it always inside window
 pygame.mouse.set_visible(False)
 pygame.event.set_grab(True)
 
@@ -119,8 +115,6 @@ def run():
             move_player_pos(1, 90)
         if keys[pygame.K_a]:
             move_player_pos(-1, 90)
-        
-        
 
 
         # Set up split screen rendering surface
@@ -223,19 +217,14 @@ def run():
                 screen_coords(zx2, zd2)], 0)
 
         # Render split screen
-
         screen.blit(split_screen, (SCREEN_WIDTH + 1, 0))
+        
+        fps_text = font.render("FPS: " + str(round(clock.get_fps(), 1)), 1, (255, 255, 255))
+        screen.blit(fps_text, (10, 5))
 
         # Update screen
-
         pygame.display.update()
         
         # Sleep
-        
-        new_time = time.time()
-        diff_time = new_time - prev_time
-        prev_time = new_time
-        if diff_time < FRAME_MIN:
-            time.sleep(FRAME_MIN - diff_time)
-
+        clock.tick(FPS)
 run()
--- src/player.py
+++ src/player.py
@@ -1,10 +1,12 @@
 """Player data"""
 
 
-
 class Player:
-	def __init__(self, nickname):
-		self.health = 100
-		self.armor = 100
-
+    def __init__(self, nickname):
+        self.health = 100
+        self.armor = 100
+        self.pos = (0.0, 0.0) 
+        self.rot = 0.0 # 0 is north, clockwise, degrees
+        self.nickname = nickname
+        print(f'{self.nickname} welcome!')
 
